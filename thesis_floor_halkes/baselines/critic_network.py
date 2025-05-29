9fcff2f19cb5dc876ae86c5ce97d01f207cbb890
Jens van de Zande <jensvandezande@gmail.com>
2025-05-30 00:36:51 +0200

    add dropout

--- thesis_floor_halkes/baselines/critic_network.py
+++ thesis_floor_halkes/baselines/critic_network.py
@@ -4,16 +4,18 @@ import torch.nn.functional as F
 
 
 class CriticBaseline(nn.Module):
-    def __init__(self, input_dim, hidden_dim=128, hidden_layers=2):
+    def __init__(self, input_dim, hidden_dim=128, hidden_layers=2, dropout=0.1):
         super().__init__()
         layers = []
         # Input layer
         layers.append(nn.Linear(input_dim, hidden_dim))
         layers.append(nn.ReLU())
+        layers.append(nn.Dropout(dropout))
         # Hidden layers
         for _ in range(hidden_layers - 1):
             layers.append(nn.Linear(hidden_dim, hidden_dim))
             layers.append(nn.ReLU())
+            layers.append(nn.Dropout(dropout))
         # Output layer
         layers.append(nn.Linear(hidden_dim, 1))
         self.critic = nn.Sequential(*layers)
